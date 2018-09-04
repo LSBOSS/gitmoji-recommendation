cc1e45db12b584629957d9f5e8b9df0c77c293dd
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-09-04 23:17:45 +0200

    :sparkles: cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -1,49 +1,44 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode"
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// this method is called when your extension is activated (very first time the command is executed)
 export function activate(context: vscode.ExtensionContext) {
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    "extension.sayHello",
+    async () => {
+      const picked = await vscode.window.showQuickPick(
+        ["wrench", "bug", "hammer"].map(s => `:${s}:`),
+        { canPickMany: false }
+      )
+      const gitExtension = vscode.extensions.getExtension("vscode.git")
+      if (gitExtension) {
+        const extension = await gitExtension.activate()
+        const model = extension._model
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "gitmoji-recommendation" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', async () => {
-        const picked = await vscode.window.showQuickPick(["wrench", "bug", "hammer"].map(s => `:${s}:`), { canPickMany: false })
-        const git = vscode.extensions.getExtension("vscode.git")
-        if (git) {
-          //git.exports
-          const extension = await git.activate()
-          const model = extension._model
-
-          //const ws = (vscode.workspace.workspaceFolders || [{ uri: { fsPath: "" }}])[0]
-          //const wsFolder = ws.uri.fsPath
-          //const repo = await model._openRepository(wsFolder)
-          const openRepo = model.openRepositories[0].repository
-          if (openRepo) {
-            const sourceControl: vscode.SourceControl = openRepo.sourceControl || openRepo._sourceControl
-            sourceControl.inputBox.value = ":boom: finally have it!"
-          }
+        // Load repository via model
+        // const ws = (vscode.workspace.workspaceFolders || [{ uri: { fsPath: "" }}])[0]
+        // const wsFolder = ws.uri.fsPath
+        // const repo = await model._openRepository(wsFolder)
+        const openRepo = model.openRepositories[0].repository
+        if (openRepo) {
+          const sourceControl: vscode.SourceControl =
+            openRepo.sourceControl || openRepo._sourceControl
+          sourceControl.inputBox.value = ":boom: finally have it!"
         }
+      }
 
-        //const gitSCM = vscode.scm.createSourceControl('git', "Git");
-        //gitSCM.rootUri
-        // gitSCM.inputBox.value = ":hankey: Deine Muddah";
-        // gitSCM.dispose()
-        // Display a message box to the user
-        vscode.window.showInformationMessage(`Hello World! ${picked}`);
-    });
+      // Create a new SCM Instance
+      // const gitSCM = vscode.scm.createSourceControl("git", "Git");
+      // gitSCM.rootUri
+      // gitSCM.inputBox.value = ":hankey: Deine Muddah";
+      // gitSCM.dispose()
+      // Display a message box to the user
+      vscode.window.showInformationMessage(`Hello World! ${picked}`)
+    }
+  )
 
-    context.subscriptions.push(disposable);
-
+  context.subscriptions.push(disposable)
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
