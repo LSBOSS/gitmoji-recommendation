99ba185e2fed47aae5f50984da45f451dd7af6d9
Christoph Reinbothe <c_k_r@gmx.de>
2018-10-30 14:28:52 +0100

    :sparkles: Getting actual diff per changed file

--- src/utils.ts
+++ src/utils.ts
@@ -1,25 +1,58 @@
+import { SourceControl, CancellationToken, Uri, CancellationTokenSource } from "vscode"
 import { GitResourceGroup } from "./gitinterfaces";
 
-function printResourceGroup(group: GitResourceGroup) {
+interface GitUriParams {
+  path: string;
+  ref: string;
+  submoduleOf?: string;
+}
+
+function fromGitUri(uri: Uri): GitUriParams {
+  return JSON.parse(uri.query);
+}
+
+
+async function printResourceGroup(repo: any, group: GitResourceGroup) {
+  const token: CancellationToken = new CancellationTokenSource().token
   group.resourceStates.length > 0
-    ? group.resourceStates.forEach(element => { console.log(`    ${element.resourceUri}`) })
+    ? await Promise.all(group.resourceStates.map(
+      async element => {
+
+
+        try {
+          const diffR: string = await repo.diffWithHEAD(element.resourceUri.fsPath)
+          console.log(`    ${element.resourceUri}`)
+          diffR.split("\n").forEach(l => {
+            console.log(`    ${l}`)
+          })
+          console.log()
+        } catch (err) {
+          console.error(err)
+        }
+
+        // const srcCtrl: SourceControl = repo.sourceControl
+        // if(srcCtrl.quickDiffProvider && srcCtrl.quickDiffProvider.provideOriginalResource) {
+        //   const result = await srcCtrl.quickDiffProvider.provideOriginalResource(element.resourceUri, token)
+        //   console.log(`    +--${result}`)
+        // }
+      }))
     : console.log("    Nothing")
 
   console.log("\n")
 }
 
-export function printRepository(repo:any) {
+export async function printRepository(repo: any) {
   const changes = repo.workingTreeGroup as GitResourceGroup
   const stagedChanges = repo.indexGroup as GitResourceGroup
   const mergeChanges = repo.mergeGroup as GitResourceGroup
 
   console.log("Changes:")
-  printResourceGroup(changes)
+  await printResourceGroup(repo, changes)
 
   console.log("Staged Changes:")
-  printResourceGroup(stagedChanges)
+  await printResourceGroup(repo, stagedChanges)
 
   console.log("Merge Changes:")
-  printResourceGroup(mergeChanges)
+  await printResourceGroup(repo, mergeChanges)
 
 }
